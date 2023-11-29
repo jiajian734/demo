7bdd9ff465509a8530f0f7d47512e2a48b5721ce
jiajian734 <50985079+jiajian734@users.noreply.github.com>
2023-11-29 17:02:25 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
 print("hello world")
+
+
+
+from selenium import webdriver
+
+from  time import sleep
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(20)
+        # 访问”Login“ 页面
+driver.get("http://150.109.156.47:8000/")
+driver.maximize_window()
+sleep(5)
